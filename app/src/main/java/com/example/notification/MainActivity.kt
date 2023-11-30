b46167c9265059aa49d7fb7ab1d6e703de4b350e
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-11-30 08:55:24 +0700

    add notification

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.example.notification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnNotif.setOnClickListener{
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.baseline_notifications_24)
+                .setContentTitle("Notifikasi Saya")
+                .setContentText("Hello World")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    channelId,
+                    "Notifku",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                with(notificationManager) {
+                    createNotificationChannel(channel)
+                    notify(0, builder.build())
+                }
+            } else {
+                notificationManager.notify(0, builder.build())
+            }
+        }
     }
 }
